98f7054aeb61ba7f78eb96bf96f5742de2d2de23
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-16 11:17:39 +0200

    created members page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import './App.scss';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageBooks } from './pages/PageBooks';
+import { PageMembers } from './pages/PageMembers';
 import { PageAbout } from './pages/PageAbout';
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Personal Site</h1>
+			<h1>Book Club</h1>
 			<hr />
 			<NavLink to="/welcome">Welcome</NavLink> |{' '}
 			<NavLink to="/books">Books</NavLink> |{' '}
+			<NavLink to="/members">Members</NavLink> |{' '}
 			<NavLink to="/about">About</NavLink>
 			<hr />
 			<Routes>
 				<Route path="/welcome" element={<PageWelcome />} />
 				<Route path="/books" element={<PageBooks />} />
+				<Route path="/members" element={<PageMembers />} />
 				<Route path="/about" element={<PageAbout />} />
 				<Route path="/" element={<Navigate to="/welcome" replace />}/>
 			</Routes>
---
+++ src/pages/PageMembers.jsx
@@ -0,0 +1,8 @@
+export const PageMembers = () => {
+	return (
+		<>
+			<h2>Members</h2>
+			<p>This is the members page.</p>
+		</>
+	);
+};
