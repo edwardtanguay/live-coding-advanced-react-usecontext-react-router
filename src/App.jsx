634058b4884c0761248b85964af0dab5c39a2a38
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-16 11:30:11 +0200

    books loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,20 @@ import { PageBooks } from './pages/PageBooks';
 import { PageMembers } from './pages/PageMembers';
 import { PageAbout } from './pages/PageAbout';
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const bookUrl = 'https://edwardtanguay.netlify.app/share/books.json';
 
 function App() {
+	const [books, setBooks] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			setBooks((await axios.get(bookUrl)).data);
+		})();
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>Book Club</h1>
@@ -16,7 +28,7 @@ function App() {
 			<NavLink to="/about">About</NavLink>
 			<hr />
 			<Routes>
-				<Route path="/welcome" element={<PageWelcome />} />
+				<Route path="/welcome" element={<PageWelcome books={books} />} />
 				<Route path="/books" element={<PageBooks />} />
 				<Route path="/members" element={<PageMembers />} />
 				<Route path="/about" element={<PageAbout />} />
--- src/pages/PageWelcome.jsx
+++ src/pages/PageWelcome.jsx
@@ -1,8 +1,9 @@
-export const PageWelcome = () => {
+export const PageWelcome = ({books}) => {
 	return (
 		<div className="page_welcome">
 			<h2>Welcome</h2>
 			<p>Welcome to this site.</p>
+			<p>We have read {books.length} books.</p>
 		</div>
 	)
 }
