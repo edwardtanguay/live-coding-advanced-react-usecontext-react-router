7430b74af2f3520c28b69c302cc0829ce2492826
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-16 11:38:39 +0200

    books styled

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -7,7 +7,16 @@ export const PageBooks = ({ books }) => {
 				{books.map((book, index) => {
 					return (
 						<div className="book" key={index}>
-							<div className="title">{book.title}</div>
+							<img
+								src={`https://edwardtanguay.netlify.app/share/images/books/${book.idcode}.png`}
+								alt="book cover"
+							/>
+							<div className="info">
+								<div className="title">{book.title}</div>
+								<div className="description">
+									{book.description}
+								</div>
+							</div>
 						</div>
 					);
 				})}
